d920cfc1393075e9d4cde62318a09bb2c03e8f20
leeyunhome <leeyunhome@daum.net>
2020-04-22 18:07:29 +0900

    add GuGuDan.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import GuGudan from "./components/GuGudan";
 
 const App = () => {
   return (
-    <div>
+    <>
       hello world
-    </div>
+      <GuGudan />
+    </>
   );
 };
 
---
+++ src/components/GuGudan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GuGudan = () => {
+    return (
+        <div>
+            hello, hooks
+        </div>
+    );
+};
+
+export default GuGudan;
